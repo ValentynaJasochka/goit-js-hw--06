74dfea73dcef1202c48a2a05df1d7d838a8b195a
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2023-06-14 23:01:36 +0300

    second attempt

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,13 @@
 const userName = document.querySelector("#name-input");
 const outPut = document.querySelector("#name-output");
+const firstOutPut = outPut.textContent;
 userName.addEventListener("input", onInput);
 
 function onInput(evt) {
   const userName = evt.currentTarget.value.trim();
-  if (userName) {
-    return (outPut.textContent = userName);
+  if (!userName) {
+    outPut.textContent = firstOutPut;
+  } else {
+    outPut.textContent = userName;
   }
 }
